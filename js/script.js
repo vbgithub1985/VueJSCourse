b720df2278fc624e5e6823e58088b5f0fe70d1f1
vbrechko <vbrechko@onwardtek.com>
2025-04-16 23:34:03 +0200

    Blank page

--- js/script.js
+++ js/script.js
@@ -1,18 +1,11 @@
 const { createApp } = Vue;
         createApp({
             setup(){
-                const date = new Date().toLocaleString();
-                const grettings = 'Hello guys, today is ' +  date;
-                const isTrue = true;
-                const array = ['Francis','Jane'];
-                const obj = {car:'Ferrari'}
+                
 
 
                 return {
-                    grettings,
-                    isTrue,
-                    array,
-                    obj
+                    
                 }
             }
         }).mount('#app')
