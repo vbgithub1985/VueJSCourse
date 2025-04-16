8f60e42226da0fff631fdd3a94e65e8ca6e62c28
vbrechko <vbrechko@onwardtek.com>
2025-04-17 00:38:04 +0200

    Conditional-rendering

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
-const { createApp } = Vue;
+const { createApp, ref } = Vue;
         createApp({
             setup(){
+                const user = ref({
+                    name:'Alex',
+                    age: 20,
+                    allowedAge:18
+                })
+
+                function checkUser(){
+                    return user.name==='Viktor' ? true:false
+                }
+
                 
 
-
                 return {
-                    
+                    user,
+                    checkUser
                 }
             }
         }).mount('#app')
