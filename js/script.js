b16bad161f93cbc640459eee59c1c510341b0d48
vbrechko <vbrechko@onwardtek.com>
2025-04-22 23:07:52 +0200

    Loopers

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-const { createApp } = Vue;
+const { createApp, ref } = Vue;
         createApp({
             setup(){
-                
+                const names = ref(['Helen','Viktor','Kyryl']);
+                const cars = ref([
+                    {brand:'Ferrari',color:'red'},
+                    {brand:'Mercedes',color:'black'},
+                    {brand:'hyundai',color:'Violet'}
+                ]);
+                const user = {
+                    name:'Fransis',
+                    lastname:'Jones',
+                    age:22
+                }
 
 
                 return {
-                    
+                    names,
+                    cars,
+                    user
                 }
             }
         }).mount('#app')
