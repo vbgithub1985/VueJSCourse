1bcbeb6e7d83e40b4f0c16d9135ceb98a4fce683
vbrechko <vbrechko@onwardtek.com>
2025-04-23 22:06:26 +0200

    re-render

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-const { createApp } = Vue;
+const { createApp, ref } = Vue;
         createApp({
             setup(){
-                
+                const name = ref('Francis');
 
+                function update(){
+                    setTimeout(()=>{
+                        name.value = 'Steve';
+                    },2000)
+                }
 
                 return {
-                    
+                    name,
+                    update
                 }
             }
         }).mount('#app')
