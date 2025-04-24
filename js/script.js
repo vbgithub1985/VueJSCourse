2b537876045ab94a6fcd25336b0e5c6e7553eed4
vbrechko <vbrechko@onwardtek.com>
2025-04-25 00:10:17 +0200

    Working with Events

--- js/script.js
+++ js/script.js
@@ -2,10 +2,26 @@ const { createApp, ref } = Vue;
         createApp({
             setup(){
                 
+                const name = ref('Viktor');
+                const coord = ref({x:0,y:0});
+                function updateName(){
+                    
+                    if (name.value == 'Helen')
+                        name.value = 'Viktor'
+                    else if (name.value == 'Viktor')
+                        name.value = 'Helen';
+                }
 
+                function getCoord(event){
+                    coord.value.x = event.clientX;
+                    coord.value.y = event.clientY;
+                }
 
                 return {
-                    
+                    name,
+                    updateName,
+                    coord,
+                    getCoord
                 }
             }
         }).mount('#app')
