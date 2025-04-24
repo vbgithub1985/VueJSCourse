2816a5018541be016d60a131f94d61259bef4bf7
vbrechko <vbrechko@onwardtek.com>
2025-04-25 00:35:04 +0200

    Events arguments

--- js/script.js
+++ js/script.js
@@ -4,8 +4,26 @@ const { createApp, ref } = Vue;
                 
 
 
-                return {
+                const name = ref('Viktor');
+                const coord = ref({x:0,y:0});
+                function updateName(){
                     
+                    if (name.value == 'Helen')
+                        name.value = 'Viktor'
+                    else if (name.value == 'Viktor')
+                        name.value = 'Helen';
+                }
+
+                function handleForm(event){
+                    //event.preventDefault();
+                    console.log('form submitted');
+                }
+
+                return {
+                    name,
+                    updateName,
+                    coord,
+                    handleForm
                 }
             }
         }).mount('#app')
